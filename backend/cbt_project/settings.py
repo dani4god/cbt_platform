32bd242c05d171f56c3aeab2a9aa49e4bd30af31
dani4god <dokeke646@gmail.com>
2025-07-28 02:34:54 -0700

    fifth commit

--- backend/cbt_project/settings.py
+++ backend/cbt_project/settings.py
@@ -76,9 +76,9 @@ AUTHENTICATION_BACKENDS = [
 # If you need to allow credentials (cookies, HTTP authentication), set this to True.
 # We will likely need this for authentication later.
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS" , "").split(" , ")
 # cbt_platform/backend/cbt_project/settings.py
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 # In production, you will add your actual domain names here, e.g.:
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'api.yourdomain.com', 'student.yourdomain.com', 'admin.yourdomain.com']
 
